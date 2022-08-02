d281e551612b131f869db4cec150a67e7e2c46e3
KMV <9117479@gmail.com>
2022-08-02 13:00:52 +0300

    next 1

---
+++ src/Ls1.java
@@ -0,0 +1,5 @@
+public class Ls1 {
+    public static void main(String [] args){
+        System.out.println("Next step...");
+    }
+}
---
+++ src/Ls2.java
@@ -0,0 +1,27 @@
+public class Ls2 {
+    public static void main(String [] args){
+        //primitive variable types
+
+        //whole numbers
+        short shortVariable = 32767;
+        int intVariable = 1000000000;
+        long longVariable = 2000000000;
+
+        //floating point numbers
+        double doubleVariable = 100.10;
+        float floatVariable = 200.20f; //must be "f"
+
+        //symbol
+        char charVariable = 'a'; //must be ''
+
+        //byte
+        byte byteVariable = -128; // -128...127
+
+        //boolean expression
+        boolean booleanVariable = true;
+
+        String result = "shortVariable + intVariable + longVariable";
+
+        System.out.println(shortVariable);
+    }
+}
