3805783d62908195c68e26346be1b00eba18d4ab
KMV <9117479@gmail.com>
2022-08-04 15:46:03 +0300

    Start 3

--- src/ForLoopsLs5.java
+++ src/ForLoopsLs5.java
@@ -1,6 +1,6 @@
 public class ForLoopsLs5 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 8; i += 2){//block1 Variable count, start position; block2 it`s terms ;block3 it`s action
+        for (int i = 0; i <= 8; i += 2){//block1 Variable count, start position; block2 it`s terms; block3 it`s action
             System.out.println("Next step... " + i);
         }
     }//hot key "f" + tab
---
+++ src/IFLs6.java
@@ -0,0 +1,10 @@
+public class IFLs6 {
+    public static void main(String[] args) {
+        int myInt = 5; //launch first term
+        if (myInt < 10){
+            System.out.println("yeap!");
+        }else {
+            System.out.println("nope!");
+        }
+    }
+}
