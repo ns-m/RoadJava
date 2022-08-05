05bfbaa72d29b884a55a04f5dd56b494675496c3
KMV <9117479@gmail.com>
2022-08-05 16:00:40 +0300

    Scanner Class

---
+++ src/ConsoleInputDataLs6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConsoleInputDataLs6 {
+    public static void main(String[] args) {
+        String myString = "Next step..."; //packages java.lang class import in advance
+        Scanner myInput = new Scanner(System.in); //packages java.util class import manually
+        System.out.println("Input your data - ");
+        String myInputString = myInput.nextLine(); //end input line after push enter key
+        System.out.println("Your input`s data - " + myInputString);
+        System.out.println("----------------");
+        System.out.println("Input your number - ");
+        int inputIntData = myInput.nextInt();
+        System.out.println("Your input number is - " + inputIntData);
+    }
+}
