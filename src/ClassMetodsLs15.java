f536ac1611623a6c856f4ca7ec8afd99ae624b74
KMV <9117479@gmail.com>
2022-08-09 15:20:56 +0300

    Class metods

--- src/ClassMetodsLs15.java
+++ src/ClassMetodsLs15.java
@@ -1,22 +1,28 @@
-public class ClassesAndObjectsLs14 {
+public class ClassMetodsLs15 {
     public static void main(String[] args) {
 
-        AD adFirst = new AD();
+        ADLs15 adFirst = new ADLs15();
         adFirst.nameAd = "Cool motorbike";
         adFirst.priceAd = 700000;
         adFirst.descriptionAd = "Best motorbike of the world!";
-        System.out.println("Ad category motors - " + adFirst.nameAd + ", " + "Price ad - " + adFirst.priceAd + ", " + "Description - " + adFirst.descriptionAd);
+        /*System.out.println("Ad category motors - " + adFirst.nameAd + ", " + "Price ad - " + adFirst.priceAd + ", " + "Description - " + adFirst.descriptionAd);*/
+        adFirst.outputAdInfo();
 
-        AD adSecond = new AD();
+        ADLs15 adSecond = new ADLs15();
         adSecond.nameAd = "Interesting book";
         adSecond.priceAd = 1500;
         adSecond.descriptionAd = "Superman`s adventures";
-        System.out.println("Ad category books " + adSecond.nameAd + "Price ad - " + adSecond.priceAd + "Description - " + adSecond.descriptionAd);
+        /*System.out.println("Ad category books " + adSecond.nameAd + "Price ad - " + adSecond.priceAd + "Description - " + adSecond.descriptionAd);*/
+        adSecond.outputAdInfo();
     }
 }
 
-class AD{
+class ADLs15{
     String nameAd;
     int priceAd;
     String descriptionAd;
+
+    void outputAdInfo(){
+        System.out.println("Ad category motors - " + nameAd + ", " + "Price ad - " + priceAd + ", " + "Description - " + descriptionAd);
+    }
 }
