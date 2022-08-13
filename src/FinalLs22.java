35966954364c0f16d99e10f7ef2d5aa44936f5e1
KMV <9117479@gmail.com>
2022-08-13 20:56:50 +0300

    Final 2

--- src/FinalLs22.java
+++ src/FinalLs22.java
@@ -1,6 +1,10 @@
 public class FinalLs22 {
     public static void main(String[] args) {
-
+        final int CONSTANT2 = 10;
+        System.out.println(CONSTANT2);
     }
+}
 
+class FinalTest{
+    public static final int CONSTANT = 0;
 }
