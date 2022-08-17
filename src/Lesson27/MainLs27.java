f5395daedd4852b8ba526836bdd7f0b5d4a54bdb
KMV <9117479@gmail.com>
2022-08-17 16:34:40 +0300

    packages

---
+++ src/Lesson27/MainLs27.java
@@ -0,0 +1,23 @@
+package Lesson27;
+// import packages
+/*import Lesson27.Forest.Apple;
+import Lesson27.Forest.Pinecone;
+import Lesson27.Forest.Thicket.OldOak;*/
+import Lesson27.Forest.*;
+import Lesson27.Forest.Thicket.*;
+
+//import java.util.Scanner;
+import java.util.*;
+
+public class MainLs27 {
+
+    public static void main(String[] args) {
+
+        Apple yellowApple = new Apple();
+        Pinecone bigPinecone = new Pinecone();
+
+        Scanner myInput = new Scanner(System.in);
+
+        OldOak greenOak = new OldOak();
+    }
+}
