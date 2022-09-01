7992ec4bbc45201b27ba24bff510cd1311d8382b
KMV <9117479@gmail.com>
2022-09-01 17:53:09 +0300

    Anonymous Classes 1

---
+++ src/AnonymousClassesLs35.java
@@ -0,0 +1,11 @@
+public class AnonymousClassesLs35 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Star{
+    public void light(){
+        System.out.println("Star light!");
+    }
+}
