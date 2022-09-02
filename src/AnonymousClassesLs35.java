6f72301819a0b6d78dd137452c7d0c9516c25ab4
KMV <9117479@gmail.com>
2022-09-02 12:14:48 +0300

    Anonymous Classes 2

--- src/AnonymousClassesLs35.java
+++ src/AnonymousClassesLs35.java
@@ -1,11 +1,50 @@
-public class AnonymousClassesLs35 {
-    public static void main(String[] args) {
 
+/*class Star implements CanLight{
+    @Override
+    public void light() {
+        System.out.println("Star light!");
     }
+
+    *//*public void light(){
+        System.out.println("Star light!");
+    }*//*
+}*/
+
+/*
+class SuperStar extends Star{
+    public void light(){
+        System.out.println("Super star is lighting!");
+    }
+}*/
+
+interface CanLight{
+    public void light();
 }
 
-class Star{
-    public void light(){
-        System.out.println("Star light!");
+public class AnonymousClassesLs35 {
+    public static void main(String[] args) {
+        /*Star sun = new Star();
+        sun.light();*/
+        /*SuperStar superSun = new SuperStar();
+        superSun.light();*/
+
+        /*Star bigStar = new Star(){
+            public void light(){
+                System.out.println("Big star is super light...");
+            }
+        };
+        bigStar.light();*/
+
+        /*CanLight canLight = new Star();
+        canLight.light();*/
+
+        CanLight canLight = new CanLight() {
+            @Override
+            public void light() {
+                System.out.println("...White...");
+            }
+        };
+
+        canLight.light();
     }
 }
