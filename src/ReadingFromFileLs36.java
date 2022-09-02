3f7c76375cceb83f9a9ed56920e0885bb117d95b
KMV <9117479@gmail.com>
2022-09-02 17:27:59 +0300

    Reading from files 2

--- src/ReadingFromFileLs36.java
+++ src/ReadingFromFileLs36.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadingFromFileLs36 {
@@ -17,5 +18,28 @@ public class ReadingFromFileLs36 {
         }
 
         scanner.close();
+
+        String path2 = "C:" + separator + "BackUp1C" + separator + "AndroidStudioProjects" + separator + "RoadJava" + separator + "src" + separator + "testdata2.txt";
+        File filedata2 = new File(path2);
+        Scanner scanner2 = new Scanner(filedata2);
+        int counter = 0;
+
+        String lineData = scanner2.nextLine();
+        String[] strNumbers = lineData.split(" ");
+        int[] numbers = new int[3];
+
+        for (String number: strNumbers
+             ) {
+            numbers[counter++] = Integer.parseInt(number);
+        }
+
+        System.out.println(Arrays.toString(numbers));
+
+        scanner2.close();
+
+        File filedata3 = new File("testPathFile.txt");
+        Scanner scanner3 = new Scanner(filedata3);
+        String lineData2 = scanner3.nextLine();
+        System.out.println(lineData2);
     }
 }
