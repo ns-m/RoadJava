fca8404290a0346158380709af3d2d03b0bf39e2
KMV <9117479@gmail.com>
2022-09-05 17:51:21 +0300

    Exceptions 1

---
+++ src/ExceptionsLs37.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class ExceptionsLs37 {
+    public static void main(String[] args) {
+
+        File file = new File("testPath");
+
+        Scanner scanner = new Scanner(file);
+    }
+}
