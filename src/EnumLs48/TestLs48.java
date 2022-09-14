9eb3fc60cf7d8d34ffe90d60aaa288db6b53df81
KMV <9117479@gmail.com>
2022-09-14 17:47:55 +0300

    Enum 1

---
+++ src/EnumLs48/TestLs48.java
@@ -0,0 +1,12 @@
+package EnumLs48;
+
+public class TestLs48 {
+
+    private static final int DOG = 0;
+    private static final int CAT = 1;
+    private static final int COW = 2;
+
+    public static void main(String[] args) {
+
+    }
+}
